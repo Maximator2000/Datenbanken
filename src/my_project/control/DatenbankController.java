ed8bdfb65cfa696ea0d90b13caba7f6f30e9f27e
gordmaxi <gloriavibo1@gmail.com>
2020-12-07 09:21:47 +0100

    7.12

--- src/my_project/control/DatenbankController.java
+++ src/my_project/control/DatenbankController.java
@@ -2,6 +2,8 @@ package my_project.control;
 
 import KAGO_framework.control.DatabaseController;
 
+import javax.swing.*;
+
 public class DatenbankController {
     //TODO   Datenbanken anlegen und darstellen
     private DatabaseController databaseController;
@@ -11,11 +13,24 @@ public class DatenbankController {
     }
 
     private void erstelleTabellen(){
-        String sql="CREATE TABLE Fabrik(\n" +
-                "ID VARCHAR(3) NOT NULL;\n" +
-                "Name VARCHAR(40) NOT NULL\n" +
+        String sql="IF NOT EXIST( SELECT 1\n" +
+                "FROM db855656x2810214.TABLES\n" +
+                "WHERE TABLE_NAME='MG_Fabrik')\n" +
+                "CREATE TABLE MG_Fabrik(\n" +
+                "ID VARCHAR(3) NOT NULL\n"+
+                "Name VARCHAR(40) NOT NULL\n"+
                 "PRIMARY KEY(ID)";
         databaseController.executeStatement(sql);
+    }
+
+    public void legeTabelleAn(JTable jTable){
+        int i=jTable.getColumnCount();
+        while (jTable.getColumnCount()>0){
+            jTable.removeColumn(jTable.getColumn(i));
+            i--;
+        }
+
+        String sql="SELECT MG_FABRIK *";
     }
 
 }
